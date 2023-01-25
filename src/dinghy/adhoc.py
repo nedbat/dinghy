3108fd08b12295f5a0babd2eaba29f2aba4401fd
Ned Batchelder <ned@nedbatchelder.com>
2023-01-25 17:40:25 -0500

    feat: improvements to adhoc: raw result and typed arguments

--- src/dinghy/adhoc.py
+++ src/dinghy/adhoc.py
@@ -16,20 +16,42 @@ from .graphql_helpers import GraphqlHelper
 logger = click_log.basic_config("dinghy")
 
 
+TYPES = {
+    "int": int,
+    "str": str,
+}
+
+
 @click.command()
 @click_log.simple_verbosity_option(logger)
+@click.option(
+    "--nodes",
+    is_flag=True,
+    help="Get paginated list of nodes instead of raw result",
+)
 @click.argument("query_file", type=click.File("r"))
-@click.argument("var", metavar="[VAR=VAL]...", nargs=-1)
-def adhoc(query_file, var):
+@click.argument("var", metavar="[VAR[:type]=VAL]...", nargs=-1)
+def adhoc(nodes, query_file, var):
     """
     Run an ad-hoc GraphQL query.
     """
     query = query_file.read()
-    variables = dict(v.split("=", 1) for v in var)
+    variables = {}
+    for v in var:
+        name, val = v.split("=", 1)
+        if ":" in name:
+            name, type_name = name.split(":")
+            val = TYPES[type_name](val)
+        variables[name] = val
 
     token = os.environ.get("GITHUB_TOKEN", "")
     gql = GraphqlHelper("https://api.github.com/graphql", token)
-    data, _ = main_run(gql.nodes(query=query, variables=variables, clear_nodes=False))
+    if nodes:
+        data, _ = main_run(
+            gql.nodes(query=query, variables=variables, clear_nodes=False)
+        )
+    else:
+        data = main_run(gql.execute(query=query, variables=variables))
     json.dump(data, sys.stdout, indent=2)
 
 
